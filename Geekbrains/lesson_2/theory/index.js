24bd3436fbb63dedf0dbcc327c7db5920928ea68
Evgenii Tatarenko <tatarenkoe@gmail.com>
2024-12-10 15:16:08 +0700

    lesson 2 hw

--- Geekbrains/lesson_2/theory/index.js
+++ Geekbrains/lesson_2/theory/index.js
@@ -1,21 +1,72 @@
-const { log } = require('console');
-const { EventEmitter } = require('events');
+const { log, error } = require('console');
+const minimist = require('minimist');
+const readlne = require('readline');
+const fs = require('fs');
 
-class Kettle extends EventEmitter {
-  constructor() {
-    super();
-    process.nextTick(() => this.emit('created', {}));
-  }
 
-  start = () => {
-    setImmediate(() => this.emit('started', {}));
-    setTimeout(() => this.emit('ready', {}), 2000);
-  }
-}
+//? node index.js --start 123 true -x 3 -y5 -abc --beep=true one two
+// log(
+//   process.argv.slice(2)
+// );
 
-const k = new Kettle();
-k.start();
+// log(
+//   minimist( process.argv.slice(2) )
+// );
 
-k.on('created', () => log('kettle is created'));
-k.on('started', () => log('kettle is started'));
-k.on('ready', () => log('kettle is ready'));
+
+
+//? node index.js -l
+//? node index.js --language
+// log(
+//   minimist( process.argv.slice(2), { alias: {l: 'language'} })
+// );
+
+
+
+// log(
+//   process.env
+// );
+//? NODE_ENV=dev node index.js
+// switch ( process.env.NODE_ENV ) {
+//   case 'dev': require('dev.config');
+//     break;
+//   case 'prod':  require('prod.config');
+//     break;
+// }
+
+
+
+//? $ node index.js
+// log( 'What\'s new?' );
+// error( 'Could be better!' );
+
+// const rl = readlne.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   // error: process.error,
+// })
+
+// rl.on('line', line => {
+//   log(`Your message is '${line}'`);
+//   if ( line === 'exit' ) {
+//     rl.close();
+//   }
+// });
+
+
+
+//? $ node index.js
+// fs.exists('./package.json', exists => {
+//   if ( exists ) {
+//     fs.readFile('./package.json', (err, data) => {
+//       log(data.toString());
+//     });
+//   } 
+// })
+
+//! try not to use
+// const exists = fs.existsSync('./package.json');
+// if ( exists ) {
+//   const data = fs.readFileSync('./package.json');
+//   log(data.toString())
+// }
