d649b90a841855552b65c7af95d30f6ae84151c6
Evgenii <evtatdevelop@gmail.com>
2025-05-27 21:18:05 +0700

    lesson 8: server & form

---
+++ Geekbrains/lesson_8/index.js
@@ -0,0 +1,12 @@
+import { Server } from "socket.io";
+const io = new Server();
+
+io.on('connection', socket => {
+  socket.on('msg', (msg) => {
+    msg.date = new Date();
+    io.emit('broadcast', msg);  
+    socket.emit('msg', msg);
+  });
+});
+
+io.listen(9999);
